3febea2e602184dfa1e1d8ef761f17cf49b3204b
Mehmet <mehmetkusken@gmail.com>
2021-04-17 21:10:57 -0600

    commit tree 4a95f61e18a6d281c98fe0f1e33b9981550870c4

--- application.rb
+++ application.rb
@@ -2,9 +2,10 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
-    resp.finish
+    resp.write "Hello, my name is"
+    resp.finish 
   end
+
 
 end
 
---
+++ my_server.rb
@@ -0,0 +1,8 @@
+class MyServer
+def call(env)
+    resp = Rack::Response.new
+    resp.write "Hello"
+    resp.finish 
+  end
+end
+
